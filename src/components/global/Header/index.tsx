4050e54680f5dc76d6d993fb3e60c66664da63c7
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2022-04-01 02:38:06 +0100

    Implement Input component

--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -1,7 +1,17 @@
+import Input from '../Input';
+import { ReactComponent as SearchIcon } from 'assets/vectors/search.svg';
 import styles from './header.module.scss';
 
 const Header = () => {
-  return <header className={styles['header']}></header>;
+  return (
+    <header className={styles['header']}>
+      <Input
+        containerClass={styles['header__input']}
+        rightIcon={<SearchIcon />}
+        placeholder="Ask us any question"
+      />
+    </header>
+  );
 };
 
 export default Header;
