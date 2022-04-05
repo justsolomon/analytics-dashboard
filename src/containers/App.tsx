fecd8d858ced86d0c5f7b3a1ad86b6ea887726a5
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2022-04-05 05:33:23 +0100

    Remove unnecessary files

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,10 @@
 import TeamsAnalytics from 'pages/analytics/teams';
-import { Provider } from 'react-redux';
-import store from 'redux/store';
 import 'styles/main.scss';
 
 const App = () => (
-  <Provider store={store}>
+  <>
     <TeamsAnalytics />
-  </Provider>
+  </>
 );
 
 export default App;
